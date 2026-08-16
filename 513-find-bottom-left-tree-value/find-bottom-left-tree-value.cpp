219aaf48870a717f96df2c89b28fc81ee66063dd
Mridul Verma <mridulverma2020@gmail.com>
2026-08-16 19:26:39 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (94.58%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxdepth=-1;
+    int bottomleft=0;
+    void dfs(TreeNode* root,int currdepth){
+        if(root==NULL) return;
+        if(currdepth>maxdepth){
+            maxdepth=currdepth;
+            bottomleft=root->val;
+        }
+        dfs(root->left,currdepth+1);
+        dfs(root->right,currdepth+1);
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        
+        
+        dfs(root,0);
+        return bottomleft;
+    }
+};
