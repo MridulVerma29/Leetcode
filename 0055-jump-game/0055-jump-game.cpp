d024cd25d34d38ea08b7f2dfb3fa999ff3ce844d
Mridul Verma <mridulverma2020@gmail.com>
2026-09-22 10:53:13 -0700

    Time: 603 ms (7.06%), Space: 55.8 MB (6.03%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+bool solve(int index,vector<int>&  nums,vector<int>& dp){
+    int n=nums.size()-1;
+    if(index==n){
+        return true;
+    }
+    
+    if(index>n){
+        return false;
+    }
+    if(nums[index]==0){
+        return false;
+    }
+    if(dp[index]!=-1) return dp[index];
+    int jumpval=nums[index];
+    for(int jump=1;jump<=jumpval;jump++){
+        bool recans=solve(index+jump,nums,dp);
+        if(recans){
+            return dp[index]=true;
+        }
+    }
+    return dp[index]=false;
+    
+}
+public:
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return solve(0,nums,dp);
+    }
+};
