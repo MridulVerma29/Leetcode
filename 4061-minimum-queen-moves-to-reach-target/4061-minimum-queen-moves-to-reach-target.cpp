ec8a9d2544c3fba08868d36a822ca615b4e9ae51
Mridul Verma <mridulverma2020@gmail.com>
2026-09-26 09:44:59 -0700

    Time: 0 ms (100%), Space: 50.5 MB (12.72%) - LeetHub

---
+++ 4061-minimum-queen-moves-to-reach-target/4061-minimum-queen-moves-to-reach-target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minQueenMoves(vector<int>& source, vector<int>& target) {
+        int r1=source[0],c1=source[1];
+        int r2=target[0],c2=target[1];
+        if(r1==r2 && c1==c2) return 0;
+        if(r1==r2 || c1==c2) return 1;
+        if(abs(r1-r2)==abs(c1-c2)) return 1;
+        return 2;
+    }
+};
